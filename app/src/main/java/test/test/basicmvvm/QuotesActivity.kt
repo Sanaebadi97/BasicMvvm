7657d5be90f3be8f825e1da35e2d4b203afc2829
Sana Ebadi <sanaebadi97@gmail.com>
2019-11-26 14:16:18 +0330

    init commit

---
+++ app/src/main/java/test/test/basicmvvm/QuotesActivity.kt
@@ -0,0 +1,12 @@
+package test.test.basicmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+    }
+}
